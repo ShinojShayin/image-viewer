857794dcdfd8de3fa386f7375511de072a4d4414
Shinoj <ShinojShayin@users.noreply.github.com>
2021-04-30 14:31:18 +0530

    login page functionality completed

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -23,10 +23,9 @@ const styles = (theme) => ({
   },
   form_field: {
     width: "325px",
-    marginBottom: "10px",
   },
   loginBtn: {
-    marginTop: "10px",
+    marginTop: "30px",
   },
 });
 
@@ -34,12 +33,59 @@ class Login extends Component {
   constructor() {
     super();
     this.state = {
-      usernameRequired: "dispNone",
+      usernameRequired: "dispHide",
       username: "",
       loginPasswordRequired: "dispNone",
       loginPassword: "",
+      incorrectPassword: "dispNone",
     };
   }
+
+  inputUsernameChangeHandler = (e) => {
+    this.setState({ username: e.target.value });
+  };
+
+  inputPasswordChangeHandler = (e) => {
+    this.setState({ loginPassword: e.target.value });
+  };
+
+  loginClickHandler = () => {
+    // Following mock username and password used for login
+    let username = "admin";
+    let password = "admin";
+
+    /**
+     * Following accessToken (Created On: 29-04-2021) used for instagram api's,
+     * currently declared access-token is long-lived accesstoken which will last 60 days
+     * */
+    let accessToken =
+      "IGQVJWQVVtTnhxbGlOc0dMVlBwNlI0WkpXamg5aUdTbzdURnkxZAjBvVzhTeGN1ZATBkeEtSdzRxMGNfdWl4aVEyLWlsYTNESG1rUmdhMXRvWE9pRWlGbUdJRERWbWgyRDgxUGNTUzV3";
+
+    let isUserPresent = this.state.username === "";
+    let isPasswordPresent = this.state.loginPassword === "";
+
+    isUserPresent
+      ? this.setState({ usernameRequired: "dispBlock" })
+      : this.setState({ usernameRequired: "dispHide" });
+
+    isPasswordPresent
+      ? this.setState({ loginPasswordRequired: "dispBlock" })
+      : this.setState({ loginPasswordRequired: "dispNone" });
+
+    if (!isUserPresent && !isPasswordPresent) {
+      if (
+        this.state.username === username &&
+        this.state.loginPassword === password
+      ) {
+        this.setState({ incorrectPassword: "dispNone" });
+        sessionStorage.setItem("access-token", accessToken);
+        this.props.history.push("/home");
+      } else {
+        this.setState({ incorrectPassword: "dispBlock" });
+      }
+    }
+  };
+
   render() {
     const { classes } = this.props;
     return (
@@ -57,6 +103,7 @@ class Login extends Component {
                   id="username"
                   type="text"
                   className={classes.form_field}
+                  onChange={this.inputUsernameChangeHandler}
                 />
                 <FormHelperText className={this.state.usernameRequired}>
                   <span className="red">required</span>
@@ -69,12 +116,17 @@ class Login extends Component {
                   id="password"
                   type="password"
                   className={classes.form_field}
+                  onChange={this.inputPasswordChangeHandler}
                 />
                 <FormHelperText className={this.state.loginPasswordRequired}>
                   <span className="red">required</span>
                 </FormHelperText>
+                <FormHelperText className={this.state.incorrectPassword}>
+                  <span className="red">
+                    Incorrect username and/or password
+                  </span>
+                </FormHelperText>
               </FormControl>
-              <br />
               <br />
               <Button
                 variant="contained"
